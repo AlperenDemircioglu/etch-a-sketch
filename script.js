5907607cb0fb9426097869dd4ac16df4538877b8
alperen_demircioglu <alperen.demircioglu@gmail.com>
2022-09-04 14:56:25 +0300

    script.js mouseover now changes the div's background color to black. style.css tried to do the background color change with defining a css property for a new class. Then adding this class to the grids which the mouse has entered. Could not do it because i was using the wrong constant.

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ let divList = document.querySelectorAll(".container");
 for(i = 0; i < gridCount; i++){ 
     divList.forEach((a) => a.appendChild(grid.cloneNode(true)));
 }
+
+const backgroundGrid = document.querySelectorAll(".grid");
+
+backgroundGrid.forEach((a) => a.addEventListener("mouseenter", (e) =>{
+    e.target.style.background = "black";
+
+
+}))
