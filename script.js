6ef180bea2f0556797e6bebad15dcdf1d6c01131
alperen_demircioglu <alperen.demircioglu@gmail.com>
2022-09-05 21:05:44 +0300

    index.html it's the same. style.css tranferred features from body to the main class. Which now contains the container class. script.js Created maindiv and gave it main class. I wanted to create this class so that it holds all the container divs and i would be able to delete the children of it freely and add later at will.

--- script.js
+++ script.js
@@ -4,25 +4,61 @@ const grid = document.createElement("div");
 grid.setAttribute("class", "grid");
 const mainDiv = document.createElement("div");
 mainDiv.setAttribute("class", "main");
+document.body.appendChild(mainDiv);
+const main = document.querySelector(".main");
 
 
 let divCount = 16;
 let gridCount = divCount;
 
-for(i = 0; i < divCount; i++){
-    document.body.appendChild(div.cloneNode(true));
+if( divCount == 16){
+    for(i = 0; i < divCount; i++){
+        main.appendChild(div.cloneNode(true));
+    }
+    
+    let divList = document.querySelectorAll(".container");
+    
+    for(i = 0; i < gridCount; i++){ 
+        divList.forEach((a) => a.appendChild(grid.cloneNode(true)));
+    }
+    
+    const backgroundGrid = document.querySelectorAll(".grid");
+    
+    backgroundGrid.forEach((a) => a.addEventListener("mouseenter", (e) =>{
+        e.target.style.background = "black";
+    
+    
+    }))
+}
+const container = document.querySelectorAll(".container");
+
+function popUp(){
+    divCount = prompt("Please enter an integer for the grid size.");
+    gridCount = divCount;
+    while(main.childElementCount > 0){
+        main.childNodes.forEach(a => main.removeChild(a));
+    }
+    
+    for(i = 0; i < divCount; i++){
+        main.appendChild(div.cloneNode(true));
+    }
+    
+    let divList = document.querySelectorAll(".container");
+    
+    for(i = 0; i < gridCount; i++){ 
+        divList.forEach((a) => a.appendChild(grid.cloneNode(true)));
+    }
+    
+    const backgroundGrid = document.querySelectorAll(".grid");
+    
+    backgroundGrid.forEach((a) => a.addEventListener("mouseenter", (e) =>{
+        e.target.style.background = "black";
+    
+    
+    }))
+    
+    
 }
 
-let divList = document.querySelectorAll(".container");
-
-for(i = 0; i < gridCount; i++){ 
-    divList.forEach((a) => a.appendChild(grid.cloneNode(true)));
-}
-
-const backgroundGrid = document.querySelectorAll(".grid");
-
-backgroundGrid.forEach((a) => a.addEventListener("mouseenter", (e) =>{
-    e.target.style.background = "black";
-
-
-}))
+btn = document.querySelector(".gridSize");
+btn.addEventListener("click", popUp);
